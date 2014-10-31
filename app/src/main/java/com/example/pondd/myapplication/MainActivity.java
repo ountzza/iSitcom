b8b0e995d0b571aadefadd9c79c623edb1f32176
Pondd <suwitcha.s@olx.co.th>
2014-10-31 14:40:38 +0700

    delete old style files

--- app/src/main/java/com/example/pondd/myapplication/MainActivity.java
+++ app/src/main/java/com/example/pondd/myapplication/MainActivity.java
@@ -117,7 +117,7 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
 
     @Override
     public void onTabSelected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction fragmentTransaction) {
-
+        mViewPager.setCurrentItem(tab.getPosition());
     }
 
     @Override
